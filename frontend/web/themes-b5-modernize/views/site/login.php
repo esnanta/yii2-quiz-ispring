0c3ae320e788df7a6823b4b2785fcbf3bdc13351
Nanta Es <ombakrinai@gmail.com>
2024-09-10 14:43:17 +0700

    update login layout

--- frontend/web/themes-b5-modernize/views/site/login.php
+++ frontend/web/themes-b5-modernize/views/site/login.php
@@ -2,6 +2,7 @@
 
 /** @var yii\web\View $this */
 /** @var yii\bootstrap5\ActiveForm $form */
+
 /** @var \frontend\models\LoginParticipantForm $model */
 
 use frontend\models\LoginParticipantForm;
@@ -12,43 +13,40 @@ $this->title = Yii::t('app', 'Authentication');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 
+    <div class="container d-flex justify-content-center align-items-center min-vh-100">
+        <div class="card" style="max-width: 400px; width: 100%;">
+            <div class="card-header text-white bg-primary d-flex justify-content-between align-items-center">
+                <?= Yii::t('app', 'Sign in as Participant') ?>
+                <i class="fas fa-sign-in-alt"></i>
+            </div>
+            <div class="card-body">
+                <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
 
-<div class="card">
-    <div class="card-header text-white bg-primary">
-        <?=Yii::t('app', 'Sign in as Participant')?>
-        <span class="pull-right"><i class="fas fa-sign-in-alt"></i></span>
+                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+
+                <?= $form->field($model, 'password')->passwordInput() ?>
+
+                <div class="form-check mb-3">
+                    <?= Html::checkbox('reveal-password', false, ['id' => 'reveal-password', 'class' => 'form-check-input']) ?>
+                    <?= Html::label('Show password', 'reveal-password', ['class' => 'form-check-label']) ?>
+                </div>
+
+                <?= $form->field($model, 'token')->textInput() ?>
+
+                <?= Html::submitButton(
+                    'Login',
+                    ['class' => 'btn btn-primary w-100', 'name' => 'login-button']
+                ) ?>
+
+                <?php ActiveForm::end(); ?>
+            </div>
+        </div>
     </div>
-    <div class="card-body">
-
-        <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-
-        <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
-
-        <?= $form->field($model, 'password')->passwordInput() ?>
-
-        <?= Html::checkbox('reveal-password', false, ['id' => 'reveal-password']) ?>
-
-        <?= Html::label('Show password', 'reveal-password') ?>
-        <br>
-        <br>
-        <?= $form->field($model, 'token')->textInput() ?>
-
-        <?=
-        Html::submitButton(
-            'Login',
-            ['class' => 'btn btn-primary', 'name' => 'login-button']
-        )
-        ?>
-
-        <?php ActiveForm::end(); ?>
-    </div>
-
-</div>
 
 <?php
-    $this->registerJs(
-            "jQuery('#reveal-password')
-            .change(function(){jQuery('#loginparticipantform-password')
-            .attr('type',this.checked?'text':'password');})"
-    );
+$this->registerJs(
+    "jQuery('#reveal-password').change(function(){
+            jQuery('#loginparticipantform-password').attr('type', this.checked ? 'text' : 'password');
+        });"
+);
 ?>
